45c6d142335c8469b1d1e394109574b9f3fdf612
wpumacay <wpumacay@gmail.com>
2026-02-26 19:32:25 -0800

    Adds franka-droid setup

--- libero/libero/envs/robots/__init__.py
+++ libero/libero/envs/robots/__init__.py
@@ -1,12 +1,17 @@
 from .mounted_panda import MountedPanda
 from .on_the_ground_panda import OnTheGroundPanda
+from .franka_droid import OnTheGroundFrankaDroid, FrankaDroidGripper
 
 from robosuite.robots.single_arm import SingleArm
 from robosuite.robots import ROBOT_CLASS_MAPPING
+from robosuite.models.grippers import ALL_GRIPPERS, GRIPPER_MAPPING
 
 ROBOT_CLASS_MAPPING.update(
     {
         "MountedPanda": SingleArm,
         "OnTheGroundPanda": SingleArm,
+        "OnTheGroundFrankaDroid": SingleArm,
     }
 )
+
+GRIPPER_MAPPING.update({"FrankaDroidGripper": FrankaDroidGripper})
---
+++ libero/libero/envs/robots/franka_droid.py
@@ -0,0 +1,93 @@
+import os
+
+import numpy as np
+
+from robosuite.models.grippers.gripper_model import GripperModel
+from robosuite.models.robots.manipulators.manipulator_model import ManipulatorModel
+from robosuite.utils.mjcf_utils import xml_path_completion
+
+EVAL_ASSETS_DIR: str | None = os.getenv("EVAL_ASSETS_DIR", None)
+if EVAL_ASSETS_DIR is None:
+    raise RuntimeError(
+        "Must provide 'EVAL_ASSETS_DIR', the folder where the assets are installed by the resources manager"
+    )
+
+if not os.path.isdir(EVAL_ASSETS_DIR):
+    raise RuntimeError(f"Given 'EVAL_ASSETS_DIR={EVAL_ASSETS_DIR}' is not a valid directory")
+
+FRANKA_DROID_DIR = os.path.join(EVAL_ASSETS_DIR, "robots", "franka_droid")
+FRANKA_DROID_ROBOT_XML = os.path.join(FRANKA_DROID_DIR, "model_no_gripper.xml")
+FRANKA_DROID_GRIPPER_XML = os.path.join(FRANKA_DROID_DIR, "robotiq_2f85_v4", "2f85.xml")
+
+
+class OnTheGroundFrankaDroid(ManipulatorModel):
+    """
+    Panda is a sensitive single-arm robot designed by Franka.
+    Args:
+        idn (int or str): Number or some other unique identification string for this robot instance
+    """
+
+    def __init__(self, idn=0):
+        super().__init__(FRANKA_DROID_ROBOT_XML, idn=idn)
+
+    @property
+    def default_mount(self):
+        return None
+
+    @property
+    def default_gripper(self):
+        return "FrankaDroidGripper"
+
+    @property
+    def default_controller_config(self):
+        return "default_panda"
+
+    @property
+    def init_qpos(self):
+        return np.array([0, -1.61037389e-01, 0.00, -2.44459747e00, 0.00, 2.22675220e00, np.pi / 4])
+
+    @property
+    def base_xpos_offset(self):
+        return {
+            "bins": (-0.5, -0.1, 0),
+            "empty": (-0.6, 0, 0),
+            "table": lambda table_length: (-0.16 - table_length / 2, 0, 0),
+            "coffee_table": lambda table_length: (-0.16 - table_length / 2, 0, 0.41),
+            "living_room_table": lambda table_length: (
+                -0.16 - table_length / 2,
+                0,
+                0.42,
+            ),
+        }
+
+    @property
+    def top_offset(self):
+        return np.array((0, 0, 1.0))
+
+    @property
+    def _horizontal_radius(self):
+        return 0.5
+
+    @property
+    def arm_type(self):
+        return "single"
+
+    @property
+    def _eef_name(self):  # type: ignore
+        return "eef_link"
+
+
+class FrankaDroidGripper(GripperModel):
+    def __init__(self, idn=0):
+        super().__init__(FRANKA_DROID_GRIPPER_XML, idn=idn)
+
+    def format_action(self, action):
+        return action
+
+    @property
+    def init_qpos(self):
+        return np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    @property
+    def _important_geoms(self):
+        return {}
